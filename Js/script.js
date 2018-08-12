b72bc0965e1866b408e7d8330444a444f2b8b46c
NahlaGalal <nahlaglal@gmail.com>
2018-08-12 23:15:54 +0200

    Catazine

---
+++ Js/script.js
@@ -0,0 +1,11 @@
+/* To resize Iframe to its content*/
+function resizeIframe(e){
+    e.style.height = e.contentWindow.document.body.scrollHeight + 'px';
+    if(e.src.split('/')[4] === "header.html"){
+        $(".carousel-control")[1].style.top = e.style.height;
+        $(".carousel-control")[0].style.top = e.style.height;
+    }
+}
+
+
+
