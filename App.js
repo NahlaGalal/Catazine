630ff35e5a1f62236baca25eed0cff60a14b6e6b
Nahla Galal <nahlaglal@gmail.com>
2019-11-18 03:23:26 +0200

    Edit public folders

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connection = require('./Connections/connections');
-const userController = require('./Controllers/UserControllers')
+const mongoose = require("mongoose");
+const userController = require("./Controllers/UserControllers");
 
-connection;
 userController(app);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use('/Assets', express.static('./Assets'));
-app.use('/Css', express.static('./Css'))
-app.use('/Images', express.static('./Images'))
-app.use('/webfonts', express.static('./webfonts'))
+app.use("/public", express.static("./public"));
+// app.use("/Assets", express.static("./Assets"));
+// app.use("/Css", express.static("./Css"));
+// app.use("/Images", express.static("./Images"));
+// app.use("/webfonts", express.static("./webfonts"));
 
-app.listen('3000', function(err){
-    if(err) console.log(err);
-})
+mongoose
+  .connect("mongodb://localhost/Catazine", { useNewUrlParser: true })
+  .then(() => app.listen("3000"))
+  .catch(err => console.log(err));
